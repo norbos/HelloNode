a27df87f437eb52853bc3f62ff1fbb01cb83721c
norbos <norbitc@outlook.com>
2021-08-29 19:54:16 -0500

    Fixed message typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.get('/', function (req, res) {
     console.log(`Requesting now from: ${req.hostname}`);
-    res.send('Hello World! updated Message');
+    res.send('Hello World! Updated Message');
 });
 
 app.listen(8000, function () {
